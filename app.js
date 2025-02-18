50848d1baf98817e6cf196e28a517b1212cc1bb2
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-02-18 22:44:34 +0500

    add image

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ var seconds_html = document.getElementById('seconds')
 var now = new Date()
 var ramadan = new Date('3/01/2025')
 var diff = ramadan.getTime() - now.getTime()
-var seconds = diff / 1000   
-var mins = diff / 1000 / 60
-var hours = diff / 1000 / 60 /60
+
+var interval = setInterval(function (){
+    var currentDate = new Date()
+    diff = diff - 100
+
 var days = diff / 1000 / 60 / 60 / 24
+var hours = 24 - currentDate.getHours()
+var mins = 60 - currentDate.getMinutes()
+var seconds = 60 - currentDate.getSeconds() 
 
 
 days_html.innerText = Math.floor(days)
@@ -18,4 +23,4 @@ hours_html.innerText = Math.floor(hours)
 
 mins_html.innerText = Math.floor(mins)
 
-seconds_html.innerText = Math.floor(seconds)
+seconds_html.innerText = Math.floor(seconds)})
